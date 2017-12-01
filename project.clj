30fa467f5a693fe25a41d21e4355b8b2ed44433b
Tommi Reiman <tommi@iki.fi>
2017-12-01 08:39:53 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject c2 "0.1.0-SNAPSHOT"
   :description "Compojure-api 2.0.0 (alpha) demo"
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [metosin/compojure-api "2.0.0-alpha12"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+                 [metosin/compojure-api "2.0.0-alpha15"]
+                 [metosin/ring-swagger-ui "2.2.10"]
                  [manifold "0.1.6"]
                  [metosin/spec-tools "0.5.1"]]
   :ring {:handler c2.handler/app, :async? true}
