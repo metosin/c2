99ff3f71525f223b6b097daca251a8e749e3aaaf
Tommi Reiman <tommi@iki.fi>
2018-03-13 20:21:29 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject c2 "0.1.0-SNAPSHOT"
   :description "Compojure-api 2.0.0 (alpha) demo"
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
-                 [metosin/compojure-api "2.0.0-alpha16"]
-                 [metosin/spec-tools "0.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [metosin/compojure-api "2.0.0-alpha19"]
+                 [metosin/spec-tools "0.6.1"]
                  [manifold "0.1.6"]]
   :ring {:handler c2.handler/app, :async? true}
   :uberjar-name "server.jar"
-  :profiles {:dev {:plugins [[lein-ring "0.12.1"]]}})
+  :profiles {:dev {:plugins [[lein-ring "0.12.3"]]}})
